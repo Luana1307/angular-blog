0533f49af3081843c207c4eb53385078c3edbfa0
Luana <luana.ssantana98@outlook.com>
2022-11-26 10:38:00 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "description": "Marvel anuncia novo homem de ferro",
+        "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/EAEA7A6A30CF7AF96370D97FEACA1ABBFBBF4D837AB24391566BE5C67F71DF8E/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+        "id":"2",
+        "title": "Nova série anunciada no Disney+",
+        "description": "ola",
+        "photoCover": "https://www.comicsuniverse.it/wp-content/uploads/2022/06/IMG_20220619_124251_450.jpg"
+    }
+]
+   
